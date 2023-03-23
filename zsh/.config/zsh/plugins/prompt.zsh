6a5c81f87bb8f91da6bd5e114c1472644eedb003
harishnkr <hari2menon1234@gmail.com>
2023-03-23 14:52:46 +0530

    fix zsh prompt

--- zsh/.config/zsh/plugins/prompt.zsh
+++ zsh/.config/zsh/plugins/prompt.zsh
@@ -3,5 +3,5 @@
 
 setopt PROMPT_SUBST
 source $ZDOTDIR/plugins/gitprompt.zsh
-export PROMPT=$'\n'' %F{magenta}<%n@%m>%f %F{yellow}[%1~]%f'$'\n'' %# '
+export PROMPT=$'\n'' %F{magenta}<%n@%m>%f %F{yellow}[%0~]%f'$'\n'' %# '
 RPROMPT=' %F{cyan}%t%f $(git_prompt_string)'
